9becdbaa930d8daf12ad166cc11cad344f1a8918
monseroad <monsepino@gmail.com>
2016-11-08 23:57:07 -0300

    se incluye funcion para guardar tarjeta

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 
 function anadirTareas(){
+
     // esta es la caja donde dejaremos
     var contenedorExistente = document.getElementById("agregarLista");
+
     // Esta es la caja donde irá mi input y mi boton
     var cajaInicioTarea = document.createElement('div');
+
+    cajaInicioTarea.setAttribute("class", "alinear")
+
     //Creamos input y boton
     var textcont =document.createElement('input');
     var boton =document.createElement('button');
+
     //Agregamos atributos a input y boton
     textcont.setAttribute("class", "form-control");
     boton.setAttribute("class", "btn");
@@ -22,5 +28,26 @@ function anadirTareas(){
     cajaInicioTarea.appendChild(boton);
 
     contenedorExistente.appendChild(cajaInicioTarea);
+
+}
+
+function lista(){
+
+    var tituloLista = document.getElementById("agregarLista");
+    var texto = document.getElementByTagName("input");
+    var crearTexto = document.createTextNode("escribir");
+
+    var anotar = document.createElement("p");
+       anotar.appendChild("crearTexto");
+
+    var botonTarjeta = document.createElement("button");
+    var textoTarjeta = document.createTextNode("agregar tarjeta");   
+
+       botonTarjeta.appendChild("textoTarjeta");
+       botonTarjeta.setAttribute("class", "btn-primary");
+
+    tituloLista.appendChild("anotar");
+    tituloLista.appendChild("botonTarjeta");
+
 
 }
